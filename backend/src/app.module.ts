d4e243927ec3490aa5f89d9228d2029382d82478
Mateusz Piasecki <piasecki.mateusz@interia.pl>
2026-01-22 19:01:05 +0100

    finish

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
-// backend/src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'build'),
+    }),
     PrismaModule,
     ProductsModule,
     OrdersModule,
